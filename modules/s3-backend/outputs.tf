8b964ada70408ea91108a3592ce36d919a7d9b2c
Anatolii Hrebynakha <hrebynakha@gmail.com>
2025-11-01 18:07:15 +0200

    add drafts

---
+++ modules/s3-backend/outputs.tf
@@ -0,0 +1,10 @@
+output "s3_bucket_name" {
+  description = "S3 bucket name for state storage"
+  value       = s3_backend.s3_bucket_name
+}
+
+output "dynamodb_table_name" {
+  description = "DynamoDB table name for state locking"
+  value       = s3_backend.dynamodb_table_name
+}
+
