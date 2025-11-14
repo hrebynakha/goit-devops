f21429617baa8b0e69cc7673127fd9c9c70167e8
Anatolii Hrebynakha <hrebynakha@gmail.com>
2025-11-14 08:45:02 +0200

    add test hw

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
+data "aws_eks_cluster" "eks" {
+  name = module.eks.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.eks_cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+
+
 module "s3_backend" {
   source      = "./modules/s3-backend"
-  bucket_name = "test-django-app-lesson-7"
+  bucket_name = "test-django-app-lesson-8-9"
   table_name  = "terraform-locks"
 }
 
@@ -11,23 +29,33 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
   private_subnets    = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
   availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  vpc_name           = "test-lesson-7-vpc"
+  vpc_name           = "test-lesson-8-9-vpc"
 }
 
 
 module "ecr" {
   source       = "./modules/ecr"
-  ecr_name     = "lesson-7-ecr"
+  ecr_name     = "lesson-8-9-ecr"
   scan_on_push = true
 }
 
 
 module "eks" {
   source        = "./modules/eks"
-  cluster_name  = "eks-lesson-7-ecr"
+  cluster_name  = "eks-lesson-8-9-ecr"
   subnet_ids    = module.vpc.public_subnets
   instance_type = "t3.micro"
   desired_size  = 2
   max_size      = 4
   min_size      = 2
+}
+
+
+module "jenkins" {
+  source       = "./modules/jenkins"
+  cluster_name = module.eks.eks_cluster_name
+  kubeconfig   = "~/kube/config"
+  providers = {
+    helm = helm
+  }
 }
